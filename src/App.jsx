ec2ae51fda8e1bc1bb59ac43d3fdc153beedd195
swiftybo <leungjordan28@gmail.com>
2025-11-03 17:21:53 +0000

    All components created in their own jsx file and initial friends list added to app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
 import { useState } from "react";
 
+let firstFriends = [
+  {
+    id: 282,
+    name: "Anthony",
+    img: "https://i.pravatar.cc/60?{img=7}",
+    balance: -7,
+  },
+  {
+    id: 93,
+    name: "Sarah",
+    img: "https://i.pravatar.cc/60?{img=38}",
+    balance: 20,
+  },
+  {
+    id: 389,
+    name: "Clark",
+    img: "https://i.pravatar.cc/60?{img=22}",
+    balance: 0,
+  },
+];
+
 export default function App() {}
